00e93809acdebde6721512ecbe5c9c86720eb617
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-07 05:25:40 +0300

    About sayfasına veritabanından veri çekme

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelProje.Models.Siniflar;
+
+namespace TravelProje.Controllers
+{
+    public class AboutController : Controller
+    {
+        Context contex = new Context();
+        // GET: About
+        public ActionResult Index()
+        {
+            var deger = contex.Hakkimizdas.ToList();
+            return View(deger);
+        }
+    }
+}
