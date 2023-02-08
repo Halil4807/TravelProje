6ead25c1081e1604af480fce603a5bc14be0aea8
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-09 01:49:37 +0300

    Blog Detay sayfasında yorumları gösterme

---
+++ Models/Siniflar/BlogYorum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+namespace TravelProje.Models.Siniflar
+{
+    public class BlogYorum
+    {
+        public IEnumerable<Blog> DegerBlog { get; set; }
+        public IEnumerable<Yorumlar> DegerYorumlar { get; set; }
+    }
+}
