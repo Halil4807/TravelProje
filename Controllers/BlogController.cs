53f770e98fb9dffe550d66e8eae4d93de8839cc5
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-08 06:21:45 +0300

    Blogları web sitesinden alma

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelProje.Models.Siniflar;
 
 namespace TravelProje.Controllers
 {
     public class BlogController : Controller
     {
         // GET: Blog
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var blog = c.Blogs.ToList();
+            return View(blog);
         }
     }
 }
