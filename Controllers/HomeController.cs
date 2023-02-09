fe4407a0b21a40984558f50cc9f5b7886bb4fc05
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-09 03:38:27 +0300

    Slider veritabanından çekme ve üzerine gelince açıklama yazdırma

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,26 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelProje.Models.Siniflar;
 
 namespace TravelProje.Controllers
 {
     public class HomeController : Controller
     {
+
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var bloglar= c.Blogs.ToList();
+            return View(bloglar);
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            //ViewBag.Message = "Your contact page.";
 
             return View();
         }
