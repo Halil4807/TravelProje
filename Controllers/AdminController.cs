9410bbd33cee5fa7df0ec5fb93fe0d14d5e4b90b
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-11 03:12:25 +0300

    Admin Paneline Blogların Listelenmesi

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelProje.Models.Siniflar;
 
 namespace TravelProje.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
         }
     }
 }
