bbdab7792fcc633dd6e7ee6b1a6ff245dc69e7da
Halil ÖZTÜRK <34791177+Halil4807@users.noreply.github.com>
2023-02-18 01:17:53 +0300

    Admin Login işleminin yapılması

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using TravelProje.Models.Siniflar;
 
 namespace TravelProje.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        Context c = new Context();
         public ActionResult Index()
         {
             return View();
@@ -16,6 +19,21 @@ namespace TravelProje.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            var bilgiler = c.Admins.FirstOrDefault(x => x.Kullanici == admin.Kullanici && x.Sifre == admin.Sifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
+                Session["Kullanici"] = bilgiler.Kullanici.ToString();
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
